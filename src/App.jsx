141fa787ccbced22e3a691a8964c85007377c63d
sivakct <sivaak950@gmail.com>
2023-08-22 16:57:07 +0530

    working

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import MovieBooking from './components/MovieBooking'
+
+function App() {
+
+  return (
+      <div className='App'>
+       <MovieBooking/>
+      </div>
+     
+  )
+}
+
+export default App
